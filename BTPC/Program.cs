b12f0ac11c85c7faff4e72e4453ed7b7488de86d
alex.white <alex.white@taxis-4u.co.uk>
2023-10-15 16:11:30 +0200

    15.1/10/2023

--- BTPC/Program.cs
+++ BTPC/Program.cs
@@ -5,10 +5,16 @@ using System.Net;
 // TODO work on the second parameter being wildcard including subdirectories
 
 string ipAddress = args[0];
-string fileName = args[1];	
+string fileName = args[1];
+bool wildcard = false;
+
+if (fileName.IndexOf("*", 0) > -1) {
+	wildcard = true;
+}
+
 IPAddress addr = IPAddress.Parse(ipAddress);
-Client client = new Client();
-MD5 md5 = new MD5();    
+Client client = new();
+MD5 md5 = new();
 
 System.IO.FileInfo fi = new FileInfo(fileName);
 UploadFile uploadFile = new();
@@ -16,7 +22,5 @@ uploadFile.FileLength = (int)fi.Length;
 uploadFile.FileName = fileName;
 uploadFile.RelativePath = false;
 uploadFile.Checksum = md5.Checksum(fileName);
-//uploadFile.ComputerName = "WebServer";
 uploadFile.ComputerName = System.Environment.MachineName;
 client.StartClient(uploadFile, ipAddress);
-
